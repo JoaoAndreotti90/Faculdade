ce62bd3fecfba3d782241cfe708b903da0f66ae3
JoaoAndreotti90 <joaogilberto238@gmail.com>
2025-07-07 09:29:41 -0300

    Update cadastro.js

--- php/js/cadastro.js
+++ php/js/cadastro.js
@@ -1,20 +1,16 @@
-// Adiciona um evento de clique ao botão "Clientes"
+
 document.getElementById("clientesBtn").addEventListener("click", function() {
-    // Redireciona para o arquivo clientes.php
     window.location.href = "./clientes/clientes.php";
 });
 
 document.getElementById("vendedoresBtn").addEventListener("click", function() {
-    window.location.href = "./vendedores/vendedores.php"; // Caminho correto para vendedores
+    window.location.href = "./vendedores/vendedores.php"; 
 });
 
 document.getElementById("produtosBtn").addEventListener("click", function() {
-    window.location.href = "./produtos/produtos.php"; // Caminho correto para produtos
+    window.location.href = "./produtos/produtos.php"; 
 });
 
-
-// Adiciona um evento de clique ao botão "Voltar"
 document.getElementById("voltarBtn").addEventListener("click", function() {
-    // Redireciona para o arquivo index.php
     window.location.href = "../index.php";
 });
